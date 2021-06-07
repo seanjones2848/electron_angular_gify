7582e4fc8bbe69aabeb5e0c268c0008c1b57d5a9
Sean Jones <seanjones2848@gmail.com>
2021-06-07 14:51:38 -0700

    added in basic angular giphy random search

---
+++ giphy.js
@@ -0,0 +1,27 @@
+var apikey = 'dc6zaTOxFJmzC';
+
+function GitController($scope, $http) {
+  $scope.images = {};
+  $scope.loading = true;
+
+  $scope.updateSearch = function () {
+    $http({
+      method: 'GET',
+      url:
+        'https://api.giphy.com/v1/gifs/search?q=' +
+        $scope.query +
+        '&api_key=' +
+        apikey,
+    })
+      .success(function (data, status) {
+        $scope.images = data;
+        console.log(data);
+
+        $scope.loading = false;
+      })
+      .error(function (data, status) {
+        $scope.status = 'Error getting repositories ! ' + data;
+        $scope.loading = false;
+      });
+  };
+}
